d1030187f73d9afc3ff64af8db4d5e7fc28e9ee8
panchoidegamer2 <pancho.filippini@gmail.com>
2024-10-15 15:49:16 -0300

    media del header y funcion del javascript en todas las paginas
    el header ya se ve bien, falta hacer que se cierre
    y funciona en todas las paginas sin linkear el javascript individualmente a cada una

--- Pagina/php/Maqueta/header.php
+++ Pagina/php/Maqueta/header.php
@@ -22,3 +22,24 @@ echo "<header class='site-header'>
 "
 
 ?>
+
+<script>
+  //menu 
+const menu = document.querySelector('.menuH');
+const botones = document.querySelector('.botones');
+
+// Get the modal
+var modal = document.getElementsByClassName('botones');
+
+menu.addEventListener('click',()=>{
+    
+    botones.classList.toggle("click");
+})
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+  </script>
